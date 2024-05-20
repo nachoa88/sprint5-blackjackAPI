4f2890515293104683606716a826451b8ed155f2
Nacho Albiol <nacho.albiol88@gmail.com>
2024-05-20 15:00:27 +0200

    First approach Read Users

---
+++ blackjack-api/app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index(): JsonResponse
+    {
+        $users = User::all();
+        
+        return response()->json($users);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(User $user): JsonResponse
+    {
+        $user = User::find($user->id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    public function edit(User $user)
+    {
+        //
+    }
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- blackjack-api/routes/api.php
+++ blackjack-api/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+});
