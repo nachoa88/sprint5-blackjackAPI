43d4bebb31adc81eca6f127a1e2452dd02909612
Nacho Albiol <nacho.albiol88@gmail.com>
2024-05-22 17:49:09 +0200

    Update nickname working (without Auth)

--- blackjack-api/app/Http/Controllers/UserController.php
+++ blackjack-api/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateUserRequest;
 
 class UserController extends Controller
 {
@@ -42,9 +43,26 @@ class UserController extends Controller
         //
     }
 
-    public function update(Request $request, User $user)
+    public function update(UpdateUserRequest $request, $id)
     {
-        //
+        // First, find the user by its UUID.
+        $user = User::where('uuid', $id)->first();
+
+        // If the user does not exist, return a 404 error.
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        // Then, validate the request.
+        $request->validated();
+        // Update the user's nickname, or set it to 'Anonymous' if no nickname is provided.
+        $user->nickname = $request['nickname'] ?? 'Anonymous';
+        // Save the user.
+        $user->save();
+
+        return response()->json([
+            'message' => 'Nickname modified successfully',
+            'new nickname' => $user->nickname
+        ]);
     }
 
     public function destroy(User $user)
---
+++ blackjack-api/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nickname' => [
+                function ($attribute, $value, $fail) {
+                    // If the nickname is not provided, set it to 'Anonymous'.
+                    $value = $value ?? 'Anonymous';
+                    // Get the UUID from the route.
+                    $uuid = $this->route('id');
+                    // Find the user by its UUID.
+                    $user = User::where('uuid', $uuid)->first();
+                    // If the user does not exist, return an error.
+                    if (!$user) {
+                        $fail('User not found with the UUID provided.');
+                    }
+                    // If the nickname is not 'Anonymous' and it already exists in another user with different UUID, return an error.
+                    else if ($value !== 'Anonymous' && User::where('nickname', $value)->where('uuid', '<>', $user->uuid)->exists()) {
+                        $fail('The ' . $attribute . ' has already been taken.');
+                    }
+                },
+            ],
+        ];
+    }
+}
